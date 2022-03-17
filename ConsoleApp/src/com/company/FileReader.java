12d6c8abeb1ef293e577aaef50cc5fcb1ff4f6ea
arturgarlacz <artur.garlacz@polcode.net>
2022-03-17 22:36:05 +0100

    Add crc32 calculator with wrong result (proper array)

--- ConsoleApp/src/com/company/FileReader.java
+++ ConsoleApp/src/com/company/FileReader.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class InputFileReader {
+public class FileReader {
 
     public static void main(String[] args) throws IOException {
 
