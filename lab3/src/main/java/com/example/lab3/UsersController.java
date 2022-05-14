58809036384923cef623571c5efe4b13497ff0f7
arturgarlacz <artur.garlacz@polcode.net>
2022-05-15 00:54:24 +0200

    implement user service in controller

--- lab3/src/main/java/com/example/lab3/UsersController.java
+++ lab3/src/main/java/com/example/lab3/UsersController.java
@@ -1,10 +1,12 @@
 package com.example.lab3;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,19 +19,71 @@ public class UsersController {
     private UsersService usersService;
 
     @RequestMapping("/api/users")
-    @ResponseBody
-    public UsersPage GetUsers(
+    public ResponseEntity<?> GetUsers(
             @RequestParam(name="page-number", defaultValue = "1") int pageNumber,
             @RequestParam(name="page-size", defaultValue = "20") int pageSize
     ) {
-        UsersPage usersPage = new UsersPage();
-        usersPage.setPageNumber(pageNumber);
-        usersPage.setPageSize(pageSize);
-        usersPage.setUsers(userList);
-
-        return usersPage;
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.usersService.getUsers(pageNumber, pageSize));
     }
 
+    @RequestMapping(
+            value = "/api/user/create",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> CreateUser(
+            @RequestParam(name="page-number", defaultValue = "1") int pageNumber,
+            @RequestParam(name="page-size", defaultValue = "20") int pageSize
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.usersService.getUsers(pageNumber, pageSize));
+    }
 
+    @RequestMapping(
+            value = "api/users/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity getUser(@PathVariable Integer id) throws JsonProcessingException {
+        return usersService.getUser(id);
+    }
+
+    @RequestMapping(
+            value = "api/users/{id}/update",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody UserEntity user) throws JsonProcessingException {
+        try {
+            return usersService.updateUser(id, user);
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"error\":\"true\"}");
+        }
+    }
+
+    @RequestMapping(
+            value = "api/users/{id}/remove",
+            method = RequestMethod.DELETE
+    )
+    @ResponseBody
+    public ResponseEntity deleteUser(@PathVariable Integer id)
+    {
+        try {
+            usersService.deleteUsers(id);
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"result\":\"true\"}");
+        }
+
+        return null;
+    }
 
 }
--- lab3/src/main/java/com/example/lab3/UsersService.java
+++ lab3/src/main/java/com/example/lab3/UsersService.java
@@ -1,18 +1,103 @@
 package com.example.lab3;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.HashMap;
+import java.io.*;
 
 @Service
 @Scope()
 public class UsersService {
 
+    HashMap<Long, UserEntity> userList;
+
+    @PostConstruct
+    private void onCreate() throws IOException {
+        // wczytywanie uzytkowników
+        try (FileInputStream inputStream = new FileInputStream("users.json")) {
+            File file = new File("users.json");
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            ObjectMapper objectMapper = new ObjectMapper();
+            userList = objectMapper.readValue(reader, HashMap.class);
+        }
+    }
+
+    @PreDestroy
+    private void onDestroy() throws IOException {
+        // zapisywanie uzytkowników
+        FileOutputStream file = new FileOutputStream("users.json");
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(file, StandardCharsets.UTF_8);
+        BufferedWriter writer = new BufferedWriter(outputStreamWriter);
+        ObjectMapper objectMapper = new ObjectMapper();
+        writer.write(objectMapper.writeValueAsString(userList));
+        writer.flush();
+    }
+
     public UsersPage getUsers(int pageNumber,int pageSize)
     {
-        return null;
+        UsersPage usersPage = new UsersPage();
+        usersPage.setPageNumber(pageNumber);
+        usersPage.setPageSize(pageSize);
+        usersPage.setTotalCount(userList.size());
+        usersPage.setUsers(this.userList.values());
+
+        return usersPage;
     }
+
+    public ResponseEntity getUser(Integer id) throws JsonProcessingException {
+        if (userList.get(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(userList.get(id));
+        }
+    }
+
+    public UserEntity addUser(String name, int age)
+    {
+        long newID = (long)(userList.size() + 1);
+        UserEntity newUser = new UserEntity(newID, name, age);
+        userList.put(newID, newUser);
+
+        return newUser;
+    }
+
+    public ResponseEntity updateUser(Long id, UserEntity user) throws JsonProcessingException {
+        user.setId(id);
+
+        if (userList.get(id) != null) {
+            userList.put(id, user);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(user);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    public ResponseEntity deleteUsers(Integer id) {
+        if (userList.get(id) != null) {
+            userList.remove(id);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"result\":\"true\"}");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
 }
